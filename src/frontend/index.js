7639015f6400dae543edfdb8929ea066ebe819d4
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-05-26 11:22:16 -0600

    Thunk Configuration

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,17 +1,21 @@
-/* eslint-disable quotes */
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
-import { Router } from "react-router";
-import { createBrowserHistory } from "history";
-import reducer from "./reducers";
-import App from "./routes/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import App from './routes/App';
 
 const history = createBrowserHistory();
 const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, preloadedState, composeEnhancers());
+const store = createStore(
+  reducer,
+  preloadedState,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 delete window.__PRELOADED_STATE__;
 
@@ -21,5 +25,5 @@ ReactDOM.hydrate(
       <App />
     </Router>
   </Provider>,
-  document.getElementById("app"),
+  document.getElementById('app'),
 );
